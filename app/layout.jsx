c0de2df89ce4712e13efef205536050b7bb7410e
JP <jay.patel.py@gmail.com>
2023-05-13 10:04:41 -0600

    changed styles for navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.variable} bg-gray-200`}>
         <Nav />
         {children}
       </body>
