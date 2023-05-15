0b94c452d629be75793059f073b43ee6e4c72887
JP <jay.patel.py@gmail.com>
2023-05-15 14:49:49 -0600

    added a check for logged in users

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link"
 import Login from "./Login"
+import { authOptions } from "../../pages/api/auth/[...nextauth]"
+import { getServerSession } from "next-auth"
 
 export default async function Nav() {
+  const session = await getServerSession(authOptions)
   return (
     <nav className="flex justify-between items-center py-8">
       <Link href={"/"}>
         <h1 className="font-bold text-lg">Send it</h1>
       </Link>
       <ul className="flex items-center gap-6">
-        <Login />
+        { !session?.user && <Login /> }
+        { session?.user && <h1>{ session.user.name }</h1> }
       </ul>
     </nav>
   )
